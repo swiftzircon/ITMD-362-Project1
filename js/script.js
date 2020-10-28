3c7e7afcd832d15caa07b919831fa5a5eb821d0e
Piotr Rajchel <59983531+swiftzircon@users.noreply.github.com>
2020-10-28 12:08:02 -0500

    Change to color function
    Removed unnecessary line creating a var to be checked, instead checking directly against the object.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-var color = document.getElementById("color_button");
-color.onclick = function() {
+document.getElementById("color_button").onclick = function() {
   document.body.classList.toggle("color_switch");
 }
 document.getElementById("main").onsubmit = function() {
