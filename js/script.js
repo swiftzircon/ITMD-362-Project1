f1bc2f8787054ca59ea5833f095ae21befd855c1
Piotr Rajchel <59983531+swiftzircon@users.noreply.github.com>
2020-10-28 01:55:46 -0500

    Added Unobstructive form validation
    Took multiple tries to get it working, but worked on the javascript to allow for form validation. Kept it simple and only required necessary email subscription fields, name and email.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-var name_field = document.getElementById("color_button");
-name_field.onclick = function() {
+var color = document.getElementById("color_button");
+color.onclick = function() {
   document.body.classList.toggle("color_switch");
+}
+document.getElementById("main").onsubmit = function() {
+  if (document.getElementById("name").value == "") {
+    alert("Name Field is required");
+	return false;
+  }
+  if (document.getElementById("email").value == "") {
+    alert("Email Field is required");
+	return false;
+  }
 }
